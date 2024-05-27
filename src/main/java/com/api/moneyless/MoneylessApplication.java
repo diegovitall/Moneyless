3739e7009310fe30b086ff9cd0f8261db7a948c4
Diego Vital <diego_vital@msn.com>
2024-05-27 13:35:08 -0300

    Iniciando o projeto

---
+++ src/main/java/com/api/moneyless/MoneylessApplication.java
@@ -0,0 +1,13 @@
+package com.api.moneyless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneylessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneylessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/moneyless/MoneylessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.moneyless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneylessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
